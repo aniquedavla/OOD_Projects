31c76af536d1e3d73ad3805284afa42d6df2d4dd
Anique <aniquedavla@gmail.com>
2017-04-08 07:42:30 -0700

    MailSystem

---
+++ HW3-FINAL/src/Five8/MailSystemTester.java
@@ -0,0 +1,25 @@
+package Five8;
+
+/**
+  Run a test of the mail system using two telephones to access the system
+@author MD ISLAM
+*/
+public class MailSystemTester
+{
+   /**
+     Creates two phones and connects them to the system.
+   */
+   public static void main(String[] args)
+   {
+      MailSystem system = new MailSystem(MAILBOX_COUNT);
+
+      Telephone p1 = new Telephone();
+      Telephone p2 = new Telephone();
+      Connection c1 = new Connection(system, p1);
+      Connection c2 = new Connection(system, p2);
+      p1.run(c1);
+      p2.run(c2);
+   }
+
+   private static int MAILBOX_COUNT = 20;
+}
