12491e967e0554cb8d202fb956e8898df1d3f43e
Anique <aniquedavla@gmail.com>
2017-04-08 07:43:34 -0700

    Decorator Pattern

---
+++ HW3-FINAL/src/Five12/CipherDecrypt.java
@@ -0,0 +1,13 @@
+package Five12;
+
+/**
+   An interface for encryption and decryption algorithms
+*/
+public interface CipherDecrypt
+{
+   /**
+     The decryption algorithm
+     @param cbuf contains the characters to decrypt
+   */
+  public void decrypt(char[] cbuf);
+}
